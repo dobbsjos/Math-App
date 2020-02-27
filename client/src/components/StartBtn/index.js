a88b9a0188a2db8c297b321a36ad2fd364c6e4b8
Joshua Dobbs <dobbsjos@gmail.com>
2020-02-27 12:45:32 -0500

    added the rest of the project

---
+++ client/src/components/StartBtn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const StartBtn = (props) => {
+  return (
+    <div>
+      <Button color="secondary" size="lg">Start Test</Button>
+    </div>
+  );
+}
+
+export default StartBtn;
---
+++ client/src/components/Submit/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+function Submit(props){
+
+    function storage(){
+        for(let i = 0; i < props.choices.length; ++i){
+            props.handleFormSubmit(props.choices[i], props.correctAnswers[i], props.ids[i])
+        }
+    }
+
+    return(
+        <div>
+            <Button onClick ={storage} color="Submit">submit</Button>
+        </div>
+    )
+}
+
+export default Submit;
